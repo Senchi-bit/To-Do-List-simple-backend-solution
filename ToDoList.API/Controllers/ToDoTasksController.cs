480d7b121b1af5b7fc3dd528659ab7561f2e8d68
Arsen Islyamov <arsenislyam@gmail.com>
2025-06-15 21:04:07 +0300

    Add Get Method for all tasks

---
+++ ToDoList.API/Controllers/ToDoTasksController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Application.CQRS.Query.Tasks;
+
+namespace ToDoList.API.Controllers;
+[ApiController]
+public class ToDoTasksController(ISender sender) : Controller
+{
+    private readonly ISender _sender = sender;
+
+    [HttpGet("AllTasks")]
+    public async Task<IActionResult> GetAllTasks([FromQuery]GetAllTasksQuery query, CancellationToken cancellationToken)
+    {
+        var tasks = await _sender.Send(new GetAllTasksQuery(), cancellationToken);
+        return Ok(tasks);
+    }
+}
