59cadd04e93a47800f417114c965c133793dc48e
Arsen Islyamov <arsenislyam@gmail.com>
2025-06-15 20:51:43 +0300

    Add DbContext + MediatR

---
+++ ToDoList.Application/Interfaces/Context/IApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Domain.Models;
+
+namespace ToDoList.Application.Interfaces.Context;
+
+public interface IApplicationContext
+{
+    public DbSet<ToDoTask> Tasks { get; set; }
+    
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
