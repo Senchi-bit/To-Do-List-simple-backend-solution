e996073a9802cdd8dce8d365ab204fde7ef5510d
Arsen Islyamov <arsenislyam@gmail.com>
2025-06-16 15:14:00 +0300

    Добавил свойство состояния в ToDoTask

--- ToDoList.Domain/Models/ToDoTask.cs
+++ ToDoList.Domain/Models/ToDoTask.cs
@@ -1,4 +1,6 @@
-﻿namespace ToDoList.Domain.Models;
+﻿using ToDoList.Domain.Enumerations;
+
+namespace ToDoList.Domain.Models;
 
 public class ToDoTask
 {
@@ -9,4 +11,16 @@ public class ToDoTask
     public string Description { get; set; } = string.Empty;
     
     public bool IsCompleted { get; set; }
+    
+    public ToDoTaskStates TaskState { get; set; }
+}
+
+public class ToDoTaskDto
+{
+    public int Id { get; set; }
+    
+    public string Title { get; set; } = string.Empty;
+    
+    public string Description { get; set; } = string.Empty;
+
 }
