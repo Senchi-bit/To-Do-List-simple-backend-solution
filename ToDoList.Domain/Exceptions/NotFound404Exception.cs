2c5df6add563fbbd10441d373358556ba11b2ca5
Arsen Islyamov <arsenislyam@gmail.com>
2025-06-19 14:19:21 +0300

    Add ExceptionHandler

---
+++ ToDoList.Domain/Exceptions/NotFound404Exception.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoList.Domain.Exceptions;
+
+public class NotFound404Exception : Exception
+{
+    public NotFound404Exception(string? message, int statusCode = 404) : base(message)
+    {
+        StatusCode = statusCode;
+    }
+    public int StatusCode {get; private set; }
+}
